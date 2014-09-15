3b2c8176a5f3531bf922c80f892a27322409b7bc
szogi <szogibalu@gmail.com>
2014-09-15 20:41:07 +0200

    Minor fixes on Client

--- deploy-tomcat.sh
+++ deploy-tomcat.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-mvn clean verify
+mvn verify
 echo "Copying cost-manager-services.war into Tomcat's webapps folder"
+rm -rf ${CATALINA_HOME}/webapps/cost-manager-services
 cp -f services/target/cost-manager-services.war ${CATALINA_HOME}/webapps
 echo "Copying cost-manager-client.war into Tomcat's webapps folder"
+rm -rf ${CATALINA_HOME}/webapps/cost-manager-client
 cp -f client/target/cost-manager-client.war ${CATALINA_HOME}/webapps
